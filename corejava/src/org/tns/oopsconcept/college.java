29b3907c26dddcff8734d6a388f2e16903273bac
Indumathi <Indumathi@DESKTOP-SFC5SRF>
2022-11-14 15:47:08 +0530

    first commit

--- corejava/src/org/tns/oopsconcept/college.java
+++ corejava/src/org/tns/oopsconcept/college.java
@@ -1,9 +1,23 @@
 package org.tns.oopsconcept;
 
+import java.util.LinkedList;
+
 public class college {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		// TODO Auto-generated method stub
+				LinkedList <String>list=new LinkedList<String>();
+				list.add("Lion");
+				list.add("Tiger");
+				list.add("Kangaro");
+				list.add("Cheetha");
+				list.add("Kingkong");
+				System.out.println("Animal are"+list);
+				list.add(2,"penguin");
+				list.add("amingo");
+				System.out.println(" new Animal name are"+list);
+				list.remove(3);
+				System.out.println("Animal are updated"+list);
 
 	}
 
