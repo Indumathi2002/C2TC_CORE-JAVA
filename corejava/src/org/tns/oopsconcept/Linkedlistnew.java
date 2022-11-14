cff35e2f1d22f0f55190c3d438c2dc58bc623800
Indumathi <Indumathi@DESKTOP-SFC5SRF>
2022-11-14 15:30:43 +0530

    FIRST COMMIT

---
+++ corejava/src/org/tns/oopsconcept/Linkedlistnew.java
@@ -0,0 +1,24 @@
+package org.tns.oopsconcept;
+
+import java.util.*;
+
+public class Linkedlistnew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList <String>list=new LinkedList<String>();
+		list.add("Lion");
+		list.add("Tiger");
+		list.add("Kangaro");
+		list.add("Cheetha");
+		list.add("Kingkong");
+		System.out.println("Animal are"+list);
+		list.add(2,"penguin");
+		list.add("amingo");
+		System.out.println(" new Animal name are"+list);
+		list.remove(3);
+		System.out.println("Animal are updated"+list);
+
+	}
+
+}
--- corejava/src/org/tns/oopsconcept/college.java
+++ corejava/src/org/tns/oopsconcept/college.java
@@ -3,7 +3,7 @@ package org.tns.oopsconcept;
 public class college {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
 	}
 
